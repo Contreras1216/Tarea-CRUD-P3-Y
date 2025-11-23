2deebb5f0827e64c3af2d3fde247713c99be3668
Contreras1216 <20220497@itla.edu.do>
2025-11-23 11:14:20 -0400

    remove: delete_user function to eliminate user deletion by ID

--- Backend/User.py
+++ Backend/User.py
@@ -68,17 +68,3 @@ async def put_user(user: User):
     
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = {"error ❌": "User not found"}) 
 
-
-# ======================= # Delete para eliminar un usuario por su id # ======================= #
-@app.delete("/user_delete/{user_id}")
-async def delete_user(user_id: int):
-    
-    if user_id <= 0:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail = {"error ❌": "ID cannot be less than or equal to zero"})
-    
-    for user in databese_users:
-        if user.id == user_id:
-            databese_users.remove(user)
-            return {"Usuario eliminado con exito ✅": user}
-    
-    raise HTTPException(status_code=404, detail = {"error ❌": "User not found for deletion"})
